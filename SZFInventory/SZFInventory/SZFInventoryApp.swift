8c46da4793c0da9721ad15886f8a43fa04469a6e
Julien Chen <chen_julien@hotmail.com>
2026-04-24 12:45:13 +0200

    Initialize app dependencies and SwiftData container

--- SZFInventory/SZFInventory/SZFInventoryApp.swift
+++ SZFInventory/SZFInventory/SZFInventoryApp.swift
@@ -5,13 +5,28 @@
 //  Created by Julien CHEN on 22/04/2026.
 //
 
+import SwiftData
 import SwiftUI
 
 @main
 struct SZFInventoryApp: App {
+    private let sharedModelContainer: ModelContainer
+    @State private var dependencies: AppDependencies
+
+    init() {
+        let container = makeModelContainer(inMemory: false)
+        sharedModelContainer = container
+        _dependencies = State(initialValue: AppDependencies(
+            modelContext: ModelContext(container),
+            apiClient: APIClient(configuration: .live)
+        ))
+    }
+
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            AppView()
+                .environment(dependencies)
         }
+        .modelContainer(sharedModelContainer)
     }
 }
